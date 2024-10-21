3bf16d449d338ec9007ad82048ef4cb7bbdc1385
addyraza <shadabraza9058@gmail.com>
2024-10-21 21:49:42 +0530

    mem

--- src/MemeGenerator.jsx
+++ src/MemeGenerator.jsx
@@ -12,8 +12,6 @@ const MemeGenerator = () => {
     setMeme({ url, title, author });
   };
 
-
-  
   const generateMeme = () => {
     fetch("https://meme-api.com/gimme")
       .then((response) => response.json())
@@ -28,7 +26,9 @@ const MemeGenerator = () => {
         Generate Meme
       </button>
       <h2 className="title">{meme.title || "Loading..."}</h2>
-      <div style={{width:"100%"}}>{meme.url && <img src={meme.url} alt={meme.title} />}</div>
+      <div style={{ width: "100%" }}>
+        {meme.url && <img src={meme.url} alt={meme.title} />}
+      </div>
       <div className="author">{meme.author && `Meme by: ${meme.author}`}</div>
     </div>
   );
